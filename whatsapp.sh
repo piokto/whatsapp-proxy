1f9fa1319b443cd5cc2e2bba066ae5271a76f827
Lucas <107782034+piokto@users.noreply.github.com>
2025-10-31 00:03:32 +0800

    Update whatsapp.sh

--- whatsapp.sh
+++ whatsapp.sh
@@ -5,15 +5,21 @@
 # *  使用Docker设置WhatsApp代理的脚本                     *
 # *  作者: Piokto                                          *
 # *  GitHub: https://github.com/piokto                     *
-# *  系统: Debian 12                                       *
+# *  TG:https://t.me/caryan                                      *
 # *                                                        *
 # **********************************************************
 
+# 定义颜色
+GREEN='\033[1;32m'
+YELLOW='\033[1;33m'
+RED='\033[1;31m'
+NC='\033[0m' # 没有颜色
+
 # 定义一个函数用于显示状态信息
 function status_message {
-    echo "============================"
-    echo "$1"
-    echo "============================"
+    echo -e "${GREEN}============================${NC}"
+    echo -e "${YELLOW}$1${NC}"
+    echo -e "${GREEN}============================${NC}"
 }
 
 # 更新本地包
@@ -56,3 +62,21 @@ docker run -it \
     whatsapp_proxy:1.0
 
 status_message "WhatsApp代理设置完成！"
+echo -e "${GREEN}通讯端口为5222，媒体端口为7777。${NC}"
+
+# 检查BBR状态
+function check_bbr {
+    status=$(sysctl net.ipv4.tcp_congestion_control)
+    if [[ "$status" == *"bbr"* ]]; then
+        echo -e "${GREEN}BBR已启用！${NC}"
+    else
+        echo -e "${RED}BBR未启用。您可以通过以下命令启用BBR:${NC}"
+        echo -e "${YELLOW}sudo modprobe tcp_bbr${NC}"
+        echo -e "${YELLOW}echo 'tcp_bbr' | sudo tee -a /etc/modules-load.d/modules.conf${NC}"
+        echo -e "${YELLOW}echo 'net.core.default_qdisc=fq' | sudo tee -a /etc/sysctl.conf${NC}"
+        echo -e "${YELLOW}echo 'net.ipv4.tcp_congestion_control=bbr' | sudo tee -a /etc/sysctl.conf${NC}"
+        echo -e "${YELLOW}sudo sysctl -p${NC}"
+    fi
+}
+
+check_bbr
